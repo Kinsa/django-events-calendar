f348d832a5497b6e6e934305bbbb6054d9d30969
Me Anonymous <tundebabzy@gmail.com>
2016-12-29 12:21:12 +0100

    removed use of `django.conf.urls.patterns` removed in Django 1.10

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from events.feeds import UpcomingEvents
 from events.views import event_list
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^feed/$', UpcomingEvents(), '', 'events_feed'),
     url(r'^$', event_list, {}, 'events'),
-)
+]
